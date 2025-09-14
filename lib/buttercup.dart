a3eb6cd69de6ec4437a93f12dbc4c94671747c0f
alpha-numericperson <arth.basilides@gmail.com>
2025-09-14 21:26:03 +0800

    Create buttercup.dart

---
+++ lib/buttercup.dart
@@ -0,0 +1,18 @@
+import 'powerPuff_girls.dart';
+
+class Buttercup extends PowerPuffGirls {
+  final String _uniqueSkill = "superSonicScream";
+  final double _effectDuration = 2.5; //seconds
+
+  String get uniqueSkill => _uniqueSkill;
+  double get effectDuration => _effectDuration;
+
+  Buttercup () : super(name: "Buttercup");
+
+  void usesuperSonicScream() {
+    print('Buttercup used $_uniqueSkill: to distract the target for $_effectDuration seconds.');
+}
+
+@override
+String info() => '${super.info()} - Unique Skill: $_uniqueSkill (Effect Duration: $_effectDuration s)';
+}
